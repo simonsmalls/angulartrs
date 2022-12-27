a7ccd344a3f43cfc682cde635c63dbfdbcc1fd44
Simon Hazevoets <simonsmalls568@gmail.com>
2022-12-27 11:44:32 +0100

    front

--- src/app/agenda/agenda.module.ts
+++ src/app/agenda/agenda.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { AgendaRoutingModule } from './agenda-routing.module';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { AddActivityComponent } from './add-activity/add-activity.component';
+import {MaterialModule} from "../shared/material/material.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { AddActivityComponent } from './add-activity/add-activity.component';
   ],
   imports: [
     CommonModule,
-    AgendaRoutingModule
+    AgendaRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AgendaModule { }
--- src/app/agenda/schedule/schedule.component.ts
+++ src/app/agenda/schedule/schedule.component.ts
@@ -1,28 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { Activity } from 'src/app/model/activity.model';
 import { DateDTO } from 'src/app/model/date-dto';
 import { ActivityService } from 'src/app/service/activity.service';
 import { EmployeeService } from 'src/app/service/employee.service';
 import {MatTableDataSource} from "@angular/material/table";
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from "@angular/material/core";
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+  styleUrls: ['./schedule.component.css'],
+
 })
 export class ScheduleComponent implements OnInit {
   activities:Activity[];
   dataSource = new MatTableDataSource<Activity>();
-  displayedColumns: string[] = ['project','category','edit','delete'];
+  displayedColumns: string[] = ['project','category','start time','end time','edit','delete'];
+  entityForm: FormGroup;
+  datum:string;
+
 
   constructor(
     private employeeService:EmployeeService,
     private activityService:ActivityService,
+    private _adapter: DateAdapter<any>,
+    @Inject(MAT_DATE_LOCALE) private _locale: string,
+    private router:Router,
+    private fb:FormBuilder,
               ) {
+
+
 
   }
 
   ngOnInit(): void {
+    this._locale='fr';
+    this._adapter.setLocale(this._locale);
     let user=this.employeeService.connectedUser;
     let date=new DateDTO();
     date.year=2022;
@@ -35,10 +51,11 @@ export class ScheduleComponent implements OnInit {
 
         this.dataSource.data=c;
       })
+    this.entityForm=this.fb.group({
+      date: [null],
 
-
-
-
+    })
+    this.datum=date.day+'/'+date.month+'/'+date.year;
   }
 
   connected():boolean{
@@ -46,6 +63,13 @@ export class ScheduleComponent implements OnInit {
       return false
     }
     return true;
+  }
+  add(){
+    this.router.navigate(['agenda/toevoegen'])
+  }
+
+  datePick(){
+    console.log('date is picked')
   }
 
 }
